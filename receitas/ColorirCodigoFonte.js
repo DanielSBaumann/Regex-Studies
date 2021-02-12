cda5485300bbd70c2d9d7e7678301e0b7a7c08ea
DanielSBaumann <dr4wone@gmail.com>
2021-02-12 16:40:51 -0300

    receitas regex

---
+++ receitas/ColorirCodigoFonte.js
@@ -0,0 +1,28 @@
+const aplicarCor = (txt, reg, cor) => txt.replace(reg, `<span style="color: #${cor}"><b>$1</b></span>`)
+
+const files = require('./files')
+const texto = files.read('codigofonte.html')
+
+const codeRegex = /<code>[\s\S]*<\/code>/i
+let codigo = texto.match(codeRegex)[0]
+
+//  String...
+codigo = aplicarCor(codigo, /(\".*\")/g, 'ce9178')
+
+//  palavras reservadas
+codigo = aplicarCor(codigo, /\b(package|public|class|static|if|else)\b/g, 'd857cf')
+
+//  tipos
+codigo = aplicarCor(codigo, /\b(void|int)/g, '1385e2')
+
+//  comentarios multiplas linhas
+codigo = aplicarCor(codigo, /(\/\*[\s\S]*\*\/)/g, '267703')
+
+//  comentarios de uma linha
+codigo = aplicarCor(codigo, /(\/\/.*?\n)/g, '267703')
+
+const conteudoFinal = texto.replace(codeRegex, codigo)
+
+console.log(conteudoFinal)
+
+files.write('codigofonte.html', conteudoFinal)
